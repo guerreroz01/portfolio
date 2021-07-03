269f49166845c09536c6c7d061f4b9f2e20bb3cd
Oliver Reyes <oliver.dentistry@gmail.com>
2021-07-04 00:29:13 +0200

    topbar, intro finished

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,37 @@
-import "./intro.scss"
+import {useEffect, useRef} from "react"
+import "./intro.scss";
+import {init} from "ityped";
 
 export default function Intro() {
+    const textRed = useRef();
+
+    useEffect(() =>{
+        init(textRed.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ["Developer", "Data Analyst"]
+        })
+    }, [])
+    
+
     return (
-        <div className="intro">
-            
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi there, I'm</h2>
+                    <h1>Oliver Reyes</h1>
+                    <h3>Freelance <span ref={textRed}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt=""/>
+                </a>
+            </div>
         </div>
     )
 }
