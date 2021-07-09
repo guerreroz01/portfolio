eb0455f1c0c79d1ada3863a45762a8422e6c0300
Oliver Reyes <oliver.dentistry@gmail.com>
2021-07-09 13:20:55 +0200

    last changes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import PortfolioList from "../portfolioList/PortfolioList"
 import "./portfolio.scss"
-import {contentPortfolio, 
-    designPortfolio, 
-    featuredPortfolio, 
-    mobilePortfolio, 
-    webPortfolio} from "../../data"
+import {
+    contentPortfolio,
+    designPortfolio,
+    featuredPortfolio,
+    mobilePortfolio,
+    webPortfolio
+} from "../../data"
 
 export default function Portfolio() {
     const [selected, setSelected] = useState("featured");
@@ -13,30 +15,30 @@ export default function Portfolio() {
 
     const list = [
         {
-            id:"featured",
-            title:"Featured",
+            id: "featured",
+            title: "Featured",
         },
         {
-            id:"web",
-            title:"Web App",
+            id: "web",
+            title: "Web App",
         },
         {
-            id:"mobile",
-            title:"Mobile App",
+            id: "mobile",
+            title: "Mobile App",
         },
         {
-            id:"design",
-            title:"Design",
+            id: "design",
+            title: "Design",
         },
         {
-            id:"content",
-            title:"Content",
+            id: "content",
+            title: "Content",
         },
     ]
 
     useEffect(() => {
-        
-        switch(selected){
+
+        switch (selected) {
             case "featured":
                 setData(featuredPortfolio);
                 break;
@@ -58,30 +60,32 @@ export default function Portfolio() {
 
     }, [selected])
     return (
-        <div className="portfolio" id="portfolio">
-            
-            <h1>Portfolio</h1>
-            <ul>
-               {
-                   list.map(item =>{
-                       return <PortfolioList 
-                       key={item.id}
-                       title={item.title} 
-                       active={selected===item.id}
-                       id={item.id}
-                       setSelected={setSelected}/>
-                   })
-               }
-            </ul>
-            <div className="container">
-                {
-                    data.map(item => {
-                        return <div className="item" key={item.id}>
-                        <img src={item.img} alt="" />
-                        <h3>{item.title}</h3>
-                    </div>
-                    })
-                }
+        <div className="mainContainer">
+
+            <div className="portfolio" id="portfolio">
+                <h1>Portfolio</h1>
+                <ul>
+                    {
+                        list.map(item => {
+                            return <PortfolioList
+                                key={item.id}
+                                title={item.title}
+                                active={selected === item.id}
+                                id={item.id}
+                                setSelected={setSelected} />
+                        })
+                    }
+                </ul>
+                <div className="container">
+                    {
+                        data.map(item => {
+                            return <div className="item" key={item.id}>
+                                <img src={item.img} alt="" />
+                                <h3>{item.title}</h3>
+                            </div>
+                        })
+                    }
+                </div>
             </div>
         </div>
     )
