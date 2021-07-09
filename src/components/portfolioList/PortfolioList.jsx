a58053d32c31a49cc85386da1b2230be3775b013
Oliver Reyes <oliver.dentistry@gmail.com>
2021-07-09 12:41:12 +0200

    template finished

--- src/components/portfolioList/PortfolioList.jsx
+++ src/components/portfolioList/PortfolioList.jsx
@@ -1,9 +1,9 @@
 import "./portfolioList.scss"
 
-export default function PortfolioList({title, active, setSelected, id}) {
+export default function PortfolioList({ title, active, setSelected, id }) {
     return (
-        <li className={ active ? "portfolioList active" : "portFolioList"}
-        onClick={()=> setSelected(id)}>
+        <li className={active ? "portfoLioList active" : "portfoLioList"}
+            onClick={() => setSelected(id)}>
             {title}
         </li>
     )
