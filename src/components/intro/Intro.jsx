b05157a3871b2d41e9d324ad95ff555efcc3e239
Oliver Reyes <oliver.dentistry@gmail.com>
2021-07-20 15:37:11 +0200

    foto

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,41 +1,38 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef } from "react";
 import "./intro.scss";
 import { init } from "ityped";
 
 export default function Intro() {
-    const textRed = useRef();
+  const textRed = useRef();
 
-    useEffect(() => {
-        init(textRed.current, {
-            showCursor: true,
-            backDelay: 1500,
-            backSpeed: 60,
-            strings: ["Developer", "Data Analyst"]
-        })
-    }, [])
+  useEffect(() => {
+    init(textRed.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Data Analyst"],
+    });
+  }, []);
 
-
-    return (
-        <div className="intro" id="intro">
-            <div className="left">
-                <div className="imgContainer">
-<<<<<<< HEAD
-                    <img src="assets/DSC_0013.jpg" alt="" />
-=======
-                    <img src="assets/DSC_0013.JPG" alt="" />
->>>>>>> 53d3da0dcbb4a95f2d8b495a3b556ee3cdcf0daa
-                </div>
-            </div>
-            <div className="right">
-                <div className="wrapper">
-                    <h2>Hi there, I'm</h2>
-                    <h1>Oliver Reyes</h1>
-                    <h3>Freelance <span ref={textRed}></span></h3>
-                </div>
-                <a href="#portfolio">
-                    <img src="assets/down.png" alt="" />
-                </a>
-            </div>
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/DSC_0013.JPG" alt="" />
         </div>
-    )
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Oliver Reyes</h1>
+          <h3>
+            Freelance <span ref={textRed}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
